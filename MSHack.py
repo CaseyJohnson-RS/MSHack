814d0759dc8a1588cac13f01ccf72a8b3fca9c2f
Casey <sharus.programmer@gmail.com>
2025-05-24 13:00:18 +0400

    So bad release...

--- MSHack.py
+++ MSHack.py
@@ -14,12 +14,19 @@ print("""
 while True:
 
     print("""
-    > Enter URL address of music sheet (example: https://musescore.com/user/30418467/scores/5575191)
-    > """)
+> Enter URL address of music sheet (example: https://musescore.com/user/30418467/scores/5575191)
+> """, end='')
 
     url = input()
 
-    path = mshack.save_score(url)
+    path = None
+
+    try:
+        path = mshack.save_score(url)
+    except Exception as e:
+        print("> Error: ", "something went wrong while saving the score.")
+        sleep(2)
+        exit(1)
 
     if path == None:
         print("> Something went wrong...")
